f201ed4a41252b7300facf749e4cf39be7f5b69c
strikerEng <kyrheepowell@gmail.com>
2023-02-17 13:03:58 -0800

    Print value of EOF and verify that the the decision making statement is always 0 or 1.

--- copyInputToOutput.c
+++ copyInputToOutput.c
@@ -12,6 +12,15 @@ main(){
        will return a distinctive value when there is no more input, a value that cannot be confused with any real character
     */
 
+    /* value of the EOF symbolic constant */
+    printf("The value of the EOF symbolic constant is %i\n",EOF);
+
+    /* verify that getchar() != EOF is 0 or 1 
+       can trigger a EOF with ctrl+D
+    */
+    int res = getchar() != EOF;
+    printf("Result: %d\n", res);
+
     while ((character = getchar()) != EOF){
         /* display character in the output stream */
         putchar(character);
