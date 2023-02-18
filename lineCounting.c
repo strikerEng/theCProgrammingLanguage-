f4fcad107ff4fdbf16bbce8ef59f0123aa664085
strikerEng <kyrheepowell@gmail.com>
2023-02-18 00:08:47 -0800

    Attempting to print out backspaces, tabs, and backslashes

--- lineCounting.c
+++ lineCounting.c
@@ -9,10 +9,16 @@ main(){
     while ((c = getchar()) != EOF)
         if (c == '\n')
             ++nl;
-        else if(c == '\t')
+        else if(c == '\t'){
             ++tabs;
+            printf("\\t\n");
+        }
         else if(c == ' ')
             ++blanks;
+        else if(c == '\b')
+            printf("\\b\n");
+        else if(c == '\\')
+            printf("\\\n");
     printf("New line count: %d ", nl);
     printf("Tab count: %d ", tabs);
     printf("Blanks count: %d\n", nl);
